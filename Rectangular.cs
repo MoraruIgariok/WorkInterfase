f0c6657bda9c0336633fd1f94bfb96713b5f8480
olegkovalevskii <74649658+olegkovalevskii@users.noreply.github.com>
2022-09-12 11:36:17 +0300

    задание по фигурам

--- Rectangular.cs
+++ Rectangular.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,54 +6,40 @@ using System.Threading.Tasks;
 
 namespace workToInterface
 {
-    class RectangularCount
-    {
-        public int count { get; set; } 
-        public int width { get; set; }
-        public int length { get; set; } 
-    }
+    /// <summary>
+    ///Опиши что за фигура
+    /// </summary>
     internal class Rectangular : IFigure
-    {
-       List<RectangularCount> rectsList = new List<RectangularCount>();
+	{
+		/// <summary>
+		/// Добавиь коментарии к переменным, и по идеи они приватные не радектируесые)
+		/// </summary>
+		public int width { get; set; }
+
+		//private readonly int width;
+
+		public int length { get; set; }
+		//private readonly int length;
 
         public Rectangular()
         {
-            Console.WriteLine("Cite figuri alegeti ? ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            
-            for(int i = 0; i < number; i++)
-            {
-                var rect = new RectangularCount();
-                rect.count = i;
-                Console.Write("Introduceti lungimea: ");
-                rect.length = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Introduceti latimea: ");
-                rect.width = Convert.ToInt32(Console.ReadLine());
-
-                rectsList.Add(rect);
-
-            }
             Console.WriteLine("********************************");
-            
+            ///todo тут оставить только заполнение текушей фигуры)
+            Console.Write("Introduceti lungimea: ");
+            length= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduceti latimea: ");
+            width = Convert.ToInt32(Console.ReadLine());
         }
         public decimal Area()
         {
-            foreach(var item in rectsList)
-            {
-                Console.WriteLine("Aria dreptunghiului {0} este {1} ", item.count + 1, item.length * item.width);
-            }
-            Console.WriteLine("--------------------------------------");
-            return 0;
+            ///метод возвраяшет результат расчета а не выводит его
+	        return length * width;
         }
 
         public decimal Perimeter()
         {
-            foreach (var item in rectsList)
-            {
-                Console.WriteLine("Perimetru dreptunghiului {0} este {1} ", item.count + 1, 2 * (item.length + item.width));
-            }
-            Console.WriteLine("--------------------------------------");
-            return 0;   
+	        ///метод возвраяшет результат расчета а не выводит его
+	        return 2 * (length + width);
         }
     }
 }
