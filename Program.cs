0a8defa36535a16b5ac3902d707e9bf8486213b8
MoraruIgariok <igor.moraru1992@gmail.com>
2022-09-15 16:39:19 +0300

    creare lista de obiecte

--- Program.cs
+++ Program.cs
@@ -1,70 +1,85 @@
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace workToInterface
 {
 	internal class Program
-	{
+	{	
+
 		static void Menu()
 		{
 			bool forLoop = true;
-			
-			Console.WriteLine("Cite figuri alegem? ");
+					
+			Console.WriteLine("How many figures do you want to select ?? ");
+
 			int NumberFigure = Convert.ToInt32(Console.ReadLine());
 
-			while (forLoop)
+
+			while (forLoop) 
 			{
 				var figure = CreateFigure();
 
+				var listFigure = new List<IFigure>();
+				
+				for (int i = 0; i < NumberFigure; i++)
+				{
+					listFigure.Add(CreateFigure());
+				}
+
 				if (figure != null)
-				{
-					figure.WriteFigure();
-				}
-				else
-				{
-					forLoop = false;
-				}
+					{				
+						foreach(IFigure item in listFigure)
+						{
+							Console.WriteLine("Aria {0},Perime {1}",item.Area(),item.Perimeter());
+						}
+					}
+					else
+					{
+						forLoop = false;
+					}
+				
 			}
 
 		}
 
 		static IFigure CreateFigure()
 		{
-			Console.WriteLine("\nCu ce figura lucram?\n");
-			string[] keyWord = { "Dreptunghi.", "Cerc.", "Patrat.", "Triunghi.", "Exit" };//lista de cuvinte din meniu.
-			int i = 0; //numarul liniei.
+			Console.WriteLine("\nWhat figure do you select ?\n");
+			string[] keyWord = { "Dreptunghi.", "Cerc.", "Patrat.", "Triunghi.", "Exit" };
+			int i = 0;
 			foreach (string word in keyWord)
 			{
 				i++;
 				Console.WriteLine(i + "." + word);
 			}
-			//afisarea meniului
+						
+			int keys = Convert.ToInt32(Console.ReadLine()); //option from menu
 
-			int keys = Convert.ToInt32(Console.ReadLine());//selectam figura din meniu
-			///Можно же сократить количество кода)
-			
+				switch (keys)
+				{
+					case 1:
+						return new Rectangular();
+					case 2:
+						return new Circle();
+					case 3:
+						return new Square();
+					case 4:
+						return new Triangle();
+					case 5:
+						return null;
+					default:
+						Console.WriteLine("Asa solutii nu avem, mai incercati");
+						return null;
+				}
 
-			switch (keys)
-			{
-				case 1:
-					return new Rectangular();
-				case 2:
-					return new Circle();
-				case 3:
-					return new Square();
-				case 4:
-					return new Triangle();
-				case 5:
-					return null;
-				default:
-					Console.WriteLine("Asa solutii nu avem, mai incercati");
-					return null;
-			}
 		}
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Lucram un pic? yes/no");
+
+			Console.WriteLine("Do you want to work ? yes/no");
+
 			string ok = Console.ReadLine();
 
 			if (ok == "yes")
